57900fccd48aaf8ea9ca64140067eb53852e0855
drylyraja <145629588+drylyraja@users.noreply.github.com>
2024-04-25 12:30:41 -0700

    marketdata function

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,43 @@ import { getMarketData } from "@/lib/get-market-data";
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const marketData = () => {
+  const [rowData, setRowData] = useState([])
+
+  useEffect(() => {
+    getMarketData()
+      .then((response) => response.json())
+      .then((data) => setRowData(data.rows))
+  }, [])  
+
+  const [columnDefs]: any = useState([
+    { field: 'name' },
+    { field: 'id' }
+  ]);
+
+  return (
+    <div
+      className="ag-theme-quartz"
+      style={{ height: '600px' }}>
+        <AgGridReact
+          gridId="market_grid"
+          rowData={rowData}
+          columnDefs={columnDefs}
+          style={{ height: '100%', width: '100%' }} />
+    </div>
+  )
+}
+
+
+
 
 
 export default function Home() {
-  const marketData = getMarketData()
-  console.log(marketData)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        test
-      </div>
+
     </main>
   );
 }
