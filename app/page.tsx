c559cdd0dbf824b3d1f7c63f80f7c08ab3590930
drylyraja <145629588+drylyraja@users.noreply.github.com>
2024-04-25 11:21:20 -0700

    removed grid example

--- app/page.tsx
+++ app/page.tsx
@@ -5,25 +5,6 @@ import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the 
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import { useState } from "react";
 
-const GridExample = () => {
-  // Row Data: The data to be displayed.
-  const [rowData, setRowData] = useState([
-    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-    { make: "Ford", model: "F-Series", price: 33850, electric: false },
-    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-  ]);
-  
-  // Column Definitions: Defines the columns to be displayed.
-  const [colDefs, setColDefs] = useState([
-    { field: "make" },
-    { field: "model" },
-    { field: "price" },
-    { field: "electric" }
-  ]);
- 
-  // ...
- 
- }
 
 export default function Home() {
   const marketData = getMarketData()
