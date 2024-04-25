e9e632c690d194e6ddde47cfb0125710b60ea574
drylyraja <145629588+drylyraja@users.noreply.github.com>
2024-04-25 10:18:43 -0700

    import agGridReact and examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,29 @@
+'use client'
 import { getMarketData } from "@/lib/get-market-data";
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { useState } from "react";
+
+const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+  
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" }
+  ]);
+ 
+  // ...
+ 
+ }
 
 export default function Home() {
   const marketData = getMarketData()
