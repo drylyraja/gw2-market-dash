307a20f24f2efa582f7fbc78a025dc058dfe17af
drylyraja <145629588+drylyraja@users.noreply.github.com>
2024-04-25 10:16:09 -0700

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { getMarketData } from "@/lib/get-market-data";
 
 export default function Home() {
+  const marketData = getMarketData()
+  console.log(marketData)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>{JSON.stringify(getMarketData())}</div>
+      <div>
+        
+      </div>
     </main>
   );
 }
