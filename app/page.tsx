b3beef3c15b176ebe16a53848e5838fda6ad5d14
drylyraja <145629588+drylyraja@users.noreply.github.com>
2024-04-25 19:38:06 -0700

    trying different ways to send the data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,56 @@
 'use client'
-import { MarketData } from "@/lib/market-data-grid";
+import { useEffect, useState } from "react";
+import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
+import { getMarketData } from "@/lib/get-market-data";
+import { ColDef } from 'ag-grid-community'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+
+// Row Data Interface
+interface IRow {
+  name: string;
+  buys: object;
+  sells: object;
+}
+const mData = getMarketData()
+
+// Create new GridExample component
+const GridExample = () => {
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState<IRow[]>([
+    mData
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState<ColDef<IRow>[]>([
+    { field: "name" },
+    { field: "buys" },
+    { field: "sells" },
+  ]);
+
+  // Container: Defines the grid's theme & dimensions.
+  return (
+    <div
+      className={
+        "ag-theme-quartz-dark"
+      }
+      style={{ width: "100%", height: "100%" }}
+    >
+      <AgGridReact rowData={rowData} columnDefs={colDefs} />
+    </div>
+  );
+};
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>{MarketData()}</div>
+    <main className="block mx-auto w-screen h-dvh">
+      <GridExample />
     </main>
   );
 }
